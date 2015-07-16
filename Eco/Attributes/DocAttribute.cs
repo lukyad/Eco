35cbf9a6899d29f0d9022cd2ec87e30f2328c2b2
dima <dmitry.al.lukyanov@gmail.com>
2015-07-16 14:29:13 +0300

    Comments

--- Eco/Attributes/DocAttribute.cs
+++ Eco/Attributes/DocAttribute.cs
@@ -6,6 +6,17 @@ using System.Threading.Tasks;
 
 namespace Eco
 {
+	/// <summary>
+	/// Provides annotation for a settings class or for a settings class's field.
+	/// Annotation gets included into configuration schema (only if target format supports schema definition).
+	/// Currently supported by XmlSchemaExporter only.
+	/// 
+	/// Usage: 
+	/// Can be applied to a class or to a field of any type.
+	/// 
+	/// Comatibility: 
+	/// Compatible with all other attributes.
+	/// </summary>
     [AttributeUsage(AttributeTargets.Class | AttributeTargets.Field)]
     public class DocAttribute : Attribute
     {
