d28238a3b29defbbe5c9b98e801f0d5284bf3984
dima <dmitry.al.lukyanov@gmail.com>
2015-07-20 19:07:20 +0300

    1. Allow custom FieldVisitors. 2. Add skipPostProcessing flag to the Load methods. If set to true, SettingsManager won't invoke non-critical FieldVisitors during the Load procedure. (those include all custom visitors provided by a user of the library).

--- Eco/SettingsManager.cs
+++ Eco/SettingsManager.cs
@@ -13,36 +13,42 @@ namespace Eco
 {
 	public class SettingsManager
     {
-		readonly ISerializaer _serializer;
+		readonly List<IFieldVisitor> _customLoadVisitors = new List<IFieldVisitor>();
+		readonly List<IFieldVisitor> _customSaveVisitors = new List<IFieldVisitor>();
+		readonly ISerializer _serializer;
 		readonly ISerializationAttributesGenerator _serializationAttributesGenerator;
 
-        public SettingsManager(ISerializaer serializer, ISerializationAttributesGenerator serializationAttributesGenerator)
+        public SettingsManager(ISerializer serializer, ISerializationAttributesGenerator serializationAttributesGenerator)
 		{
 			_serializer = serializer;
 			_serializationAttributesGenerator = serializationAttributesGenerator;
 			this.DefaultUsage = Usage.Optional;
         }
 
-		public ISerializaer Serializer { get { return _serializer; } }
+		public ISerializer Serializer { get { return _serializer; } }
 
 		public ISerializationAttributesGenerator SerializationAttributesGenerator { get { return _serializationAttributesGenerator; } }
 
 		public Usage DefaultUsage { get; set; }
+
+		public List<IFieldVisitor> CustomLoadVisitors { get { return _customLoadVisitors; } }
+
+		public List<IFieldVisitor> CustomSaveVisitors { get { return _customSaveVisitors; } }
 
 		public static string GetDefaultSettingsFileName<T>()
 		{
 			return typeof(T).Name + ".config";
 		}
 
-		public T Load<T>() where T : new()
+		public T Load<T>(bool skipPostProcessing = false) where T : new()
 		{
-			return this.Load<T>(GetDefaultSettingsFileName<T>());
+			return this.Load<T>(GetDefaultSettingsFileName<T>(), skipPostProcessing);
 		}
 
-		public T Load<T>(string fileName) where T : new()
+		public T Load<T>(string fileName, bool skipPostProcessing = false) where T : new()
 		{
 			using (var fileStream = File.Open(fileName, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
-				return this.Read<T>(fileStream);
+				return this.Read<T>(fileStream, skipPostProcessing);
 		}
 
 		public void Save<T>(T settings)
@@ -56,7 +62,7 @@ namespace Eco
 				this.Write(settings, fileStream);
 		}
 
-		public T Read<T>(Stream stream) where T : new()
+		public T Read<T>(Stream stream, bool skipPostProcessing = false) where T : new()
         {
 			Type rawSettingsType = SerializableTypeEmitter.GetSerializableTypeFor<T>(this.SerializationAttributesGenerator, this.DefaultUsage);
 			object rawSettings = this.Serializer.Deserialize(rawSettingsType, stream);
@@ -65,7 +71,15 @@ namespace Eco
 			var settingsMapBuilder = new SettingsMapBuilder();
 			VisitAllFieldsRecursive(sourceSettings: rawSettings, targetSettings: refinedSettings, visitor: settingsMapBuilder);
 			VisitAllFieldsRecursive(sourceSettings: rawSettings, targetSettings: refinedSettings, visitor: new ReferenceResolver(settingsMapBuilder.SettingsByIdMap));
-            return refinedSettings;
+			VisitAllFieldsRecursive(sourceSettings: rawSettings, targetSettings: refinedSettings, visitor: new RequiredFieldChecker());
+			if (!skipPostProcessing)
+			{
+				VisitAllFieldsRecursive(sourceSettings: rawSettings, targetSettings: refinedSettings, visitor: new EnvironmentVariableExpander());
+				foreach (var customVisitor in _customLoadVisitors)
+					VisitAllFieldsRecursive(sourceSettings: rawSettings, targetSettings: refinedSettings, visitor: customVisitor);
+			}
+
+			return refinedSettings;
         }
 
         public void Write<T>(T settings, Stream stream)
@@ -74,15 +88,19 @@ namespace Eco
 			object rawSettings = Activator.CreateInstance(rawSettingsType);
 			VisitAllFieldsRecursive(sourceSettings: settings, targetSettings: rawSettings, visitor: new SettingsObjectBuilder());
 			VisitAllFieldsRecursive(sourceSettings: settings, targetSettings: rawSettings, visitor: new ReferencePacker());
+			VisitAllFieldsRecursive(sourceSettings: settings, targetSettings: rawSettings, visitor: new RequiredFieldChecker());
+			foreach (var customVisitor in _customSaveVisitors)
+				VisitAllFieldsRecursive(sourceSettings: settings, targetSettings: rawSettings, visitor: new RequiredFieldChecker());
+
 			this.Serializer.Serialize(rawSettings, stream);
 		}
 
 		static void VisitAllFieldsRecursive(object sourceSettings, object targetSettings, IFieldVisitor visitor)
 		{
-			VisitAllFieldsRecursive(sourceSettings, targetSettings, visitor, new HashSet<object>());
+			VisitAllFieldsRecursive(sourceSettings.GetType().Name, sourceSettings, targetSettings, visitor, new HashSet<object>());
         }
 
-		static void VisitAllFieldsRecursive(object sourceSettings, object targetSettings, IFieldVisitor visitor, HashSet<object> visitedSettings)
+		static void VisitAllFieldsRecursive(string settingsPath, object sourceSettings, object targetSettings, IFieldVisitor visitor, HashSet<object> visitedSettings)
 		{
 			if (visitedSettings.Contains(sourceSettings)) return;
 			visitedSettings.Add(sourceSettings);
@@ -90,7 +108,8 @@ namespace Eco
 			foreach (var sourceSettingsField in sourceSettings.GetType().GetFields())
 			{
 				var targetSettingsField = targetSettings.GetType().GetField(sourceSettingsField.Name);
-				visitor.Visit(sourceSettingsField, sourceSettings, targetSettingsField, targetSettings);
+				string currentPath = SettingsPath.Combine(settingsPath, sourceSettingsField.Name);
+                visitor.Visit(currentPath, sourceSettingsField, sourceSettings, targetSettingsField, targetSettings);
 
 				object sourceSettingsValue = sourceSettingsField.GetValue(sourceSettings);
 				object targetSettingsValue = targetSettingsField.GetValue(targetSettings);
@@ -99,51 +118,20 @@ namespace Eco
 				{
 					if (sourceSettingsValue.GetType().IsSettingsType())
 					{
-						VisitAllFieldsRecursive(sourceSettingsValue, targetSettingsValue, visitor, visitedSettings);
+						VisitAllFieldsRecursive(currentPath, sourceSettingsValue, targetSettingsValue, visitor, visitedSettings);
 					}
 					else if (sourceSettingsValue.GetType().IsSettingsArrayType())
 					{
 						var sourceSettingsArray = (Array)sourceSettingsValue;
 						var targetSettingsArray = (Array)targetSettingsValue;
 						for (int i = 0; i < sourceSettingsArray.Length; i++)
-							VisitAllFieldsRecursive(sourceSettingsArray.GetValue(i), targetSettingsArray.GetValue(i), visitor, visitedSettings);
+						{
+							currentPath = SettingsPath.Combine(settingsPath, sourceSettingsField.Name, i);
+                            VisitAllFieldsRecursive(currentPath, sourceSettingsArray.GetValue(i), targetSettingsArray.GetValue(i), visitor, visitedSettings);
+						}
 					}
 				}
 			}
 		}
-
-		static IEnumerable<object> GetSettingsRecursively(object root)
-		{
-			return GetSettingsRecursively(root, new HashSet<object>());
-        }
-
-        static IEnumerable<object> GetSettingsRecursively(object settings, HashSet<object> visitedSettings)
-		{
-            if (settings == null || visitedSettings.Contains(settings)) yield break;
-			visitedSettings.Add(settings);
-			yield return settings;
-
-            foreach (var field in settings.GetType().GetFields())
-            {
-                object fieldValue = field.GetValue(settings);
-                if (fieldValue != null)
-                {
-                    if (fieldValue.GetType().IsSettingsType())
-                    {
-                        foreach (var s in GetSettingsRecursively(fieldValue))
-                            yield return s;
-                    }
-                    else if (fieldValue.GetType().IsSettingsArrayType())
-                    {
-                        var settingsArray = (object[])fieldValue;
-                        foreach (var item in settingsArray)
-                        {
-                            foreach (var s in GetSettingsRecursively(item))
-                                yield return s;
-                        }
-                    }
-                }
-            }
-        }
     }
 }
