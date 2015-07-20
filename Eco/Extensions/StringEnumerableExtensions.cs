6a4ba854652724002fcb3d6e763dc9fc18bf313b
dima <dmitry.al.lukyanov@gmail.com>
2015-07-20 18:56:57 +0300

    Move IEnumerable<string> functionality to the EnumerableStringExtensions class.

---
+++ Eco/Extensions/StringEnumerableExtensions.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eco.Extensions
+{
+	static class StringEnumerableExtensions
+	{
+		public static string WhiteSpaceSeparated(this IEnumerable<string> strs, int spacesCount = 1)
+		{
+			return strs.SeparatedBy(new string(' ', spacesCount));
+		}
+
+		public static string CommaWhiteSpaceSeparated(this IEnumerable<string> strs)
+		{
+			return strs.SeparatedBy(", ");
+		}
+
+		public static string CommaSeparated(this IEnumerable<string> strs)
+		{
+			return strs.SeparatedBy(',');
+		}
+
+		public static string DotSeparated(this IEnumerable<string> strs)
+		{
+			return strs.SeparatedBy('.');
+		}
+
+		public static string TabSeparated(this IEnumerable<string> strs)
+		{
+			return strs.SeparatedBy('\t');
+		}
+
+		public static string SeparatedBy(this IEnumerable<string> strs, char delimiter)
+		{
+			return strs.SeparatedBy(delimiter.ToString());
+		}
+
+		public static string SeparatedBy(this IEnumerable<string> strs, string delimiter)
+		{
+			return String.Join(delimiter, strs.ToArray());
+		}
+
+		public static string Concat(this IEnumerable<string> strs)
+		{
+			return String.Concat(strs.ToArray());
+		}
+	}
+}
--- Eco/Extensions/StringExtensions.cs
+++ Eco/Extensions/StringExtensions.cs
@@ -7,46 +7,6 @@ namespace Eco
 {
     public static class StringExtensions
     {
-        public static string WhiteSpaceSeparated(this IEnumerable<string> strs, int spacesCount = 1)
-        {
-            return strs.SeparatedBy(new string(' ', spacesCount));
-        }
-
-        public static string CommaWhiteSpaceSeparated(this IEnumerable<string> strs)
-        {
-            return strs.SeparatedBy(", ");
-        }
-
-        public static string CommaSeparated(this IEnumerable<string> strs)
-        {
-            return strs.SeparatedBy(',');
-        }
-
-        public static string DotSeparated(this IEnumerable<string> strs)
-        {
-            return strs.SeparatedBy('.');
-        }
-
-        public static string TabSeparated(this IEnumerable<string> strs)
-        {
-            return strs.SeparatedBy('\t');
-        }
-
-        public static string SeparatedBy(this IEnumerable<string> strs, char delimiter)
-        {
-            return strs.SeparatedBy(delimiter.ToString());
-        }
-
-        public static string SeparatedBy(this IEnumerable<string> strs, string delimiter)
-        {
-            return String.Join(delimiter, strs.ToArray());
-        }
-
-        public static string Concat(this IEnumerable<string> strs)
-        {
-            return String.Concat(strs.ToArray());
-        }
-
         public static string[] SplitAndTrim(this string str)
         {
             return SplitAndTrim(str, ',');
