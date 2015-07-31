df533f8a11a0925f436c95d62cd1993787c5ad66
dima <dmitry.al.lukyanov@gmail.com>
2015-07-31 11:27:56 +0300

    Split SettingsObjectBuilder into RawSettingsBuilder and RefinedSettingsBuilder

---
+++ Eco/FieldVisitors/RawSettingsBuilder.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Eco.Extensions;
+
+namespace Eco.FieldVisitors
+{
+    class RawSettingsBuilder : IFieldVisitor
+    {
+        readonly Dictionary<Type, Type> _typeMappings = new Dictionary<Type, Type>();
+
+        public void Visit(string fieldPath, FieldInfo refinedSettingsField, object refinedSettings, FieldInfo rawSettingsField, object rawSettings)
+        {
+            if (refinedSettingsField.IsDefined<RefAttribute>()) return;
+
+            object rawValue = null;
+            object refinedValue = refinedSettingsField.GetValue(refinedSettings);
+            if (refinedValue != null)
+            {
+                Type refinedValueType = refinedValue.GetType();
+                if (refinedValueType.IsSettingsType())
+                {
+                    rawValue = SettingsConstruction.CreateSettingsObject(refinedValue, rawSettingsField, _typeMappings);
+                }
+                else if (refinedValueType.IsSettingsArrayType())
+                {
+                    rawValue = SettingsConstruction.CreateSettingsArray((Array)refinedValue, rawSettingsField, _typeMappings);
+                }
+                else if (refinedValueType != typeof(string) && rawSettingsField.FieldType == typeof(string))
+                {
+                    rawValue = ToString(refinedSettingsField, refinedSettings);
+                }
+                else
+                {
+                    rawValue = refinedSettingsField.GetValue(refinedSettings);
+                }
+
+                if (refinedSettingsField.IsDefined<FieldMutatorAttribute>())
+                {
+                    refinedSettingsField.GetCustomAttribute<FieldMutatorAttribute>().SetRawSettingsFieldValue(rawSettingsField, rawSettings, rawValue);
+                }
+                else
+                {
+                    rawSettingsField.SetValue(rawSettings, rawValue);
+                }
+            }
+        }
+
+        static string ToString(FieldInfo sourceField, object container)
+        {
+            object value = sourceField.GetValue(container);
+            if (value != null && Nullable.GetUnderlyingType(sourceField.FieldType) != null)
+                value = sourceField.FieldType.GetProperty("Value").GetValue(value);
+
+            ConverterAttribute converter = sourceField.GetCustomAttribute<ConverterAttribute>();
+            if (converter != null)
+                return converter.ToString(value);
+            else
+                return value != null ? value.ToString() : null;
+        }
+    }
+}
---
+++ Eco/FieldVisitors/RefinedSettingsBuilder.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Eco.Extensions;
+
+namespace Eco.FieldVisitors
+{
+    class RefinedSettingsBuilder : IFieldVisitor
+    {
+        readonly Dictionary<Type, Type> _typeMappings = new Dictionary<Type, Type>();
+
+        public void Visit(string fieldPath, FieldInfo refinedSettingsField, object refinedSettings, FieldInfo rawSettingsField, object rawSettings)
+        {
+            if (refinedSettingsField.IsDefined<RefAttribute>()) return;
+
+            object rawValue = refinedSettingsField.IsDefined<FieldMutatorAttribute>() ?
+                refinedSettingsField.GetCustomAttribute<FieldMutatorAttribute>().GetRawSettingsFieldValue(rawSettingsField, rawSettings) :
+                rawSettingsField.GetValue(rawSettings);
+
+            object refinedValue = null;
+            if (rawValue != null)
+            {
+                var rawValueType = rawValue.GetType();
+                if (rawValueType.IsSettingsType())
+                {
+                    refinedValue = SettingsConstruction.CreateSettingsObject(rawValue, refinedSettingsField, _typeMappings);
+                }
+                else if (rawValueType.IsSettingsArrayType())
+                {
+                    refinedValue = SettingsConstruction.CreateSettingsArray((Array)rawValue, refinedSettingsField, _typeMappings);
+                }
+                else if (rawValueType == typeof(string) && refinedSettingsField.FieldType != typeof(string))
+                {
+                    refinedValue = FromString((string)rawSettingsField.GetValue(rawSettings), refinedSettingsField);
+                }
+                else
+                {
+                    refinedValue = rawSettingsField.GetValue(rawSettings);
+                }
+            }
+            refinedSettingsField.SetValue(refinedSettings, refinedValue);
+        }
+
+        static object FromString(string sourceStr, FieldInfo targetField)
+        {
+            if (sourceStr == null) return null;
+
+            ConverterAttribute converter = targetField.GetCustomAttribute<ConverterAttribute>();
+            if (converter != null)
+            {
+                return converter.FromString(sourceStr);
+            }
+            else
+            {
+                Type targetType = targetField.FieldType;
+                MethodInfo tryParseMethod = GetTryParseMethod(targetType);
+                var args = new object[] { sourceStr, Activator.CreateInstance(targetType) };
+                bool parsed = (bool)tryParseMethod.Invoke(null, args);
+                if (!parsed) throw new ConfigurationException("Failed to parse '{0}' from '{1}'", targetType.Name, sourceStr);
+                return args[1];
+            }
+        }
+
+        static MethodInfo GetTryParseMethod(Type container)
+        {
+            // TODO validate TryParse method
+            const string TryParseMethodName = "TryParse";
+            Type valueType = Nullable.GetUnderlyingType(container) ?? container;
+            return valueType.GetMethod(TryParseMethodName, new[] { typeof(string), valueType.MakeByRefType() });
+        }
+    }
+}
