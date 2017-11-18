dcd6c2c62131af0774c4f870bd5ace8366bb86fc
dmitry <dmitry.al.lukyanov@gmail.com>
2017-11-18 18:34:25 +0300

    Refresh IP address not often than 1 time per minute.

--- Eco/Variables/PublicIpVariable.cs
+++ Eco/Variables/PublicIpVariable.cs
@@ -9,11 +9,20 @@ namespace Eco.Variables
 {
     public class PublicIpVariable : IVariableProvider
     {
+        string _lastIp;
+        DateTime _lastUpdate;
+
         public Dictionary<string, Func<string>> GetVariables()
         {
+            var now = DateTime.Now;
+            if (now -  _lastUpdate > TimeSpan.FromMinutes(1))
+            {
+                _lastIp = new WebClient().DownloadString("http://icanhazip.com").Trim();
+                _lastUpdate = now;
+            }
             return new Dictionary<string, Func<string>>()
             {
-                { "publicIp", () => new WebClient().DownloadString("http://icanhazip.com").Trim() }
+                { "publicIp", () => _lastIp }
             }; 
         }
     }
